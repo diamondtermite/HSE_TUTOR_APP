2ee2c826bd84dd5be70f3caa81c5da78d2d47a1f
diamondtermite <hardik.m.negi@gmail.com>
2025-10-01 13:54:42 +0000

    First commit
    Co-authored-by: Kathir K <KathirK12@users.noreply.github.com>

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+
+    return <h1>Hello!</h1>
+
+}
+
+export default App;
+
+createRoot(document.getElementById('root')).render(<App />);
