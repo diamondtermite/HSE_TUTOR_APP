6b8bfb29241d4757d00ca2a06a934967358afd51
Kathir K <128328717+KathirK12@users.noreply.github.com>
2025-10-02 01:20:46 +0000

    Co-authored-by: diamondtermite <diamondtermite@users.noreply.github.com>

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,11 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # allow frontend to talk to backend
+CORS(app)
 
-@app.route('/api/hello')
-def hello():
-    return jsonify({"message": "Hello from Flask!"})
+@app.route('/api/')
+def index():
+    return jsonify({"message": "If you can see this, then Flask works"})
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
---
+++ frontend/src/inbox.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+function Inbox() {
+
+    return (
+
+        <div>
+
+            <h1>This is the messages div</h1>
+
+        </div>
+
+    );
+
+}
+
+export default Inbox;
