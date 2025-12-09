1ed1a20474057f35929270f7a73536fb63d64790
diamondtermite <hardik.m.negi@gmail.com>
2025-12-09 14:21:03 +0000

    Co-authored-by: Kathir K <KathirK12@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from "./nav.jsx";
 import Inbox from './inbox.jsx';
 import RequestsContainer from './requestContainer.jsx';
@@ -10,7 +10,7 @@ import Settings from './settings.jsx';
 function App() {
   const [message, setMessage] = useState("Loading...");
   const [currentPage, setCurrentPage] = useState("home");
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
 
   useEffect(() => {
     fetch("/api/")
@@ -26,43 +26,54 @@ const permissions = {
     canChangeSettings: true,
   };
 
-//-----------------------------------------------------------------------------
-  // Define the different pages and what they look like
-  const pages = {
-    home: permissions.canViewInbox && (
-      <>
-        <h1>{message}</h1>
-        <h2>Hello!</h2>
-        <Inbox />
-      </>
-    ),
-    search: permissions.canSearchTutors && (
-      <div>
-        <h1>Search Tutors</h1>
-        <Search id="search-tutor-input" btnId="search-tutor-button" />
-      </div>
-    ),
-    requests: permissions.canViewRequests && (
-      <div>
-        <h1>Requests</h1>
-        <RequestsContainer />``
-      </div>
-    ),
-    settings: permissions.canChangeSettings && (
-      <div>
-        <h1>Settings</h1>
-        <Settings />
-      </div>
-    ),
-  };
-//-----------------------------------------------------------------------------
+return (
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Inbox />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/requests" element={<Requests />} />
+        <Route path="/settings" element={<Settings />} />
+    </Routes>
+  </BrowserRouter>
+);
 
-  return (
-    <div className="app-div">
-      <Nav currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      {pages[currentPage] ?? pages.home}
-    </div>
-  );
+// //-----------------------------------------------------------------------------
+//   // Define the different pages and what they look like
+//   const pages = {
+//     home: permissions.canViewInbox && (
+//       <div className='wrap-div'>
+//         <h1>{message}</h1>
+//         <h2>Hello!</h2>
+//         <Inbox />
+//       </div>
+//     ),
+//     search: permissions.canSearchTutors && (
+//       <div className='wrap-div'>
+//         <h1>Search Tutors</h1>
+//         <Search id="search-tutor-input" btnId="search-tutor-button" />
+//       </div>
+//     ),
+//     requests: permissions.canViewRequests && (
+//       <div className='wrap-div'>
+//         <h1>Requests</h1>
+//         <RequestsContainer />``
+//       </div>
+//     ),
+//     settings: permissions.canChangeSettings && (
+//       <div className='wrap-div'>
+//         <h1>Settings</h1>
+//         <Settings />
+//       </div>
+//     ),
+//   };
+// //-----------------------------------------------------------------------------
+
+//   return (
+//     <div className="app-div">
+//       <Nav currentPage={currentPage} setCurrentPage={setCurrentPage} />
+//       {pages[currentPage] ?? pages.home}
+//     </div>
+//   );
 }
 
 export default App;
